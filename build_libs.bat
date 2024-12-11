8b1b5a4cc7c493e479f0e9a6189e3f26b5254872
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2024-12-10 19:22:05 -0500

    build_libs.bat: rebuild new and modified files

--- build_libs.bat
+++ build_libs.bat
@@ -4,35 +4,30 @@ SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 GOTO :EOF
 TITLE ImpLib SDK
 echo Compiling the import libraries
-echo Please, be patient - it takes some minutes to compile...
 echo.
 
-FOR /f "delims=" %%i IN ('dir /A-D /B /S src\MSVCRT\msvcrt.def src\Win32 2^>NUL ^| findstr /E /R ".*.def" ^|sort') DO CALL :FASM %%i
-
-GOTO EXIT
-:FASM
- SET F=%~n1
-
- echo Compiling lib\%F%.lib, including original thunk
- (
-  echo include '..\implib.inc'
-  echo KEEP_ORIGINAL_THUNK equ 1
-  more +1 %1
- ) >src\Win32\.tmp.asm
- bin\FASM src\Win32\.tmp.asm lib\%F%.lib
-
- echo Compiling lib\stripped\%F%.lib, without original thunk
- (
-  echo include '..\implib.inc'
-  echo KEEP_ORIGINAL_THUNK equ 0
-  more +1 %1
- ) >src\Win32\.tmp.asm
- bin\FASM src\Win32\.tmp.asm lib\stripped\%F%.lib
-
- GOTO :EOF
-
-:EXIT
-del /s src\Win32\.tmp.asm >nul 2>&1
+FOR /f "delims=" %%i IN ('dir /A-D /B /S src\MSVCRT\*.def src\Win32\*.def 2^>NUL ^| findstr /E /R ".*.def" ^|sort') DO CALL :BLD %%i
+del /s src\.tmp.asm >nul 2>&1
 pause
 ENDLOCAL
 @echo on
+GOTO :EOF
+
+:BLD
+ SET F=%~n1
+ CALL :DO_BLD %1 lib\%F%.lib 1 including
+ CALL :DO_BLD %1 lib\stripped\%F%.lib 0 without
+ GOTO :EOF
+
+:DO_BLD
+ IF NOT EXIST %2 GOTO FASM
+ XCOPY /L /D /Y %1 %2 | FINDSTR /B /C:"1 " >nul && GOTO FASM
+ GOTO :EOF
+:FASM
+ echo Compiling %2, %4 original thunk
+ (
+  echo include 'implib.inc'
+  echo KEEP_ORIGINAL_THUNK equ %3
+  more +1 %1
+ ) >src\.tmp.asm
+ bin\FASM src\.tmp.asm %2
