b22b8783ba56189f0e88ff05f8ef96678507b424
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2024-12-28 06:49:13 -0500

    MASM Win32 example

--- src/Win32/test_masm/test.asm
+++ src/Win32/test_masm/test.asm
@@ -2,14 +2,17 @@
 .model flat,stdcall
 
 includelib user32.lib
+includelib kernel32.lib
 
 MessageBoxA PROTO STDCALL :DWORD, :DWORD, :DWORD, :DWORD
+ExitProcess PROTO STDCALL :DWORD
 
 .CODE
 ttl db "Test",0
 msg db "Hello, world!",0
-
+align 4
 start:
 	invoke MessageBoxA, 0, OFFSET msg, OFFSET ttl, 0
+	invoke ExitProcess, eax
 	retn
 END start
