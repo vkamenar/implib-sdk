8f168c749c14f19cd27031fdec6b8355c286baad
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2024-12-30 18:02:07 -0500

    Add Win64 to build_libs.bat

--- build_libs.bat
+++ build_libs.bat
@@ -6,8 +6,7 @@ TITLE ImpLib SDK
 echo Compiling the import libraries
 echo.
 
-FOR /f "delims=" %%i IN ('dir /A-D /B /S src\MSVCRT\*.def src\Win32\*.def 2^>NUL ^| findstr /E /R ".*.def" ^|sort') DO CALL :BLD %%i
-del /s src\.tmp.asm >nul 2>&1
+FOR /f "delims=" %%i IN ('dir /A-D /B /S src\MSVCRT32\*.def src\Win32\*.def src\Win64\*.def 2^>NUL ^| findstr /E /R ".*.def" ^|sort') DO CALL :BLD "%%i"
 pause
 ENDLOCAL
 @echo on
@@ -15,8 +14,11 @@ GOTO :EOF
 
 :BLD
  SET F=%~n1
- CALL :DO_BLD %1 lib\%F%.lib 1 including
- CALL :DO_BLD %1 lib\stripped\%F%.lib 0 without
+ SET P=%~p1
+ FOR %%f IN ("%P:~0,-1%") DO SET P="%%~nxf"
+ SET P=%P:"=%
+ CALL :DO_BLD %1 lib\%P%\%F%.lib 1 including
+ CALL :DO_BLD %1 lib\%P%\stripped\%F%.lib 0 without
  GOTO :EOF
 
 :DO_BLD
@@ -24,10 +26,12 @@ GOTO :EOF
  XCOPY /L /D /Y %1 %2 | FINDSTR /B /C:"1 " >nul && GOTO FASM
  GOTO :EOF
 :FASM
+ SET /p impline=< %1
  echo Compiling %2, %4 original thunk
  (
-  echo include 'implib.inc'
+  echo %impline%
   echo KEEP_ORIGINAL_THUNK equ %3
   more +1 %1
- ) >src\.tmp.asm
- bin\FASM src\.tmp.asm %2
+ ) >"%~1.tmp"
+ bin\FASM "%~1.tmp" %2
+ del /s "%~1.tmp" >nul 2>&1
