fe799db41026f425a1b8402ca3e909c91cc521ac
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-03 13:48:03 -0500

    MASM64 example

---
+++ src/Win64/test_masm/build.bat
@@ -0,0 +1,32 @@
+@echo off
+SETLOCAL
+REM === CONFIG BEGIN =================================================
+
+REM If you have MASM64 installed, set the installation path:
+SET MASM=C:\Tools\MASM64
+
+REM === CONFIG END ===================================================
+TITLE ImpLib SDK
+echo Building a Win64 executable with MASM64 and either the MS linker or polink
+echo.
+IF EXIST "%MASM%\ml64.exe" GOTO MASMFOUND
+SET MASM=%MASM%\bin
+IF EXIST "%MASM%\ml64.exe" GOTO MASMFOUND
+SET MASM=%MASM%64
+IF EXIST "%MASM%\ml64.exe" GOTO MASMFOUND
+ECHO MASM not found. Please, specify the correct path, if MASM64 is installed.
+GOTO EXIT
+:MASMFOUND
+"%MASM%\ml64" /c test.asm
+SET LNKEXE="%MASM%\link"
+IF EXIST "%LNKEXE%.exe" GOTO LINKFOUND
+SET LNKEXE="%MASM%\polink"
+IF EXIST "%LNKEXE%.exe" GOTO LINKFOUND
+ECHO Linker not found.
+GOTO EXIT
+:LINKFOUND
+"%LNKEXE%" /ENTRY:start /SUBSYSTEM:CONSOLE /MACHINE:X64 /MERGE:.rdata=.text /LIBPATH:..\..\..\lib\Win64\stripped -ignore:4078 test.obj
+:EXIT
+pause
+ENDLOCAL
+@echo on
