8654b0e4cb64d66411e9ed43fa775d016e68ed66
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-03 13:50:37 -0500

    Updated NASM Win64 example

--- src/Win64/test_nasm/test.asm
+++ src/Win64/test_nasm/test.asm
@@ -3,9 +3,6 @@ default rel
 EXTERN __imp_GetStdHandle
 EXTERN __imp_WriteFile
 EXTERN __imp_ExitProcess
-%define GetStdHandle [__imp_GetStdHandle]
-%define WriteFile    [__imp_WriteFile]
-%define ExitProcess  [__imp_ExitProcess]
 
 section .text
 msg db "Hello, World!",13,10 ; CRLF to add a new line
@@ -13,14 +10,14 @@ msg db "Hello, World!",13,10 ; CRLF to add a new line
 align 8
 GLOBAL start
 start:
-	sub rsp,40 ; reserve shadow space, 16-byte stack alignment
+	sub rsp,40h ; reserve shadow space, 16-byte stack alignment
 	mov rcx,-11
-	call GetStdHandle
+	call [__imp_GetStdHandle]
 	mov rcx,rax          ; WriteFile: hFile
 	lea rdx,[msg]        ; WriteFile: lpBuffer
 	mov r8d,msg.len      ; WriteFile: nNumberOfBytesToWrite
 	lea r9,[rsp+48]      ; WriteFile: lpNumberOfBytesWritten
 	mov qword [rsp+32],0 ; WriteFile: lpOverlapped = NULL
-	call WriteFile
+	call [__imp_WriteFile]
 	xor ecx,ecx
-	call ExitProcess
+	call [__imp_ExitProcess]
