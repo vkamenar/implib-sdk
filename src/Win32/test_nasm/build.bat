b892bafe8fc345e641386eba3006bd3633e66b96
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-13 09:35:51 -0500

    Update build.bat to support GNU Linker

--- src/Win32/test_nasm/build.bat
+++ src/Win32/test_nasm/build.bat
@@ -7,7 +7,7 @@ SET NASM=C:\Tools\NASM
 
 REM === CONFIG END =======================================
 TITLE ImpLib SDK
-echo Building a Win32 executable with NASM and Polink
+echo Building a Win32 executable with NASM and Polink or LD
 echo.
 SET NASMEXE="%NASM%\nasmw"
 IF EXIST "%NASMEXE%.exe" GOTO NASMFOUND
@@ -22,11 +22,16 @@ GOTO EXIT
 :NASMFOUND
 "%NASMEXE%" -fwin32 test.asm
 SET POLINK="%NASM%\polink"
-IF EXIST "%POLINK%.exe" GOTO LINKFOUND
-ECHO Polink linker not found.
+IF EXIST "%POLINK%.exe" GOTO POLINKFOUND
+SET LDLINK="%NASM%\ld"
+IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
+ECHO No linker found (neither Polink nor LD).
 GOTO EXIT
-:LINKFOUND
+:POLINKFOUND
 "%POLINK%" /ENTRY:start /SUBSYSTEM:WINDOWS /MERGE:.data=.text /LIBPATH:..\..\..\lib\Win32\stripped test.obj kernel32.lib user32.lib
+GOTO EXIT
+:LDLINKFOUND
+"%LDLINK%" -m i386pe -subsystem windows -o test.exe -e start -L..\..\..\lib\Win32\stripped test.obj -lkernel32 -luser32
 :EXIT
 pause
 ENDLOCAL
