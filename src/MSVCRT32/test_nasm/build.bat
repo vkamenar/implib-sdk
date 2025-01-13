361314e2c122300f2f88cc1c2e49fb14a9e7b4dd
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-13 12:03:00 -0500

    Update build.bat to support GNU Linker

--- src/MSVCRT32/test_nasm/build.bat
+++ src/MSVCRT32/test_nasm/build.bat
@@ -2,13 +2,12 @@
 SETLOCAL
 REM === CONFIG BEGIN =====================================
 
-REM If you have NASM installed, set the installation path:
-SET NASM=C:\Tools\NASM
+REM If you have MinGW32 or a standalone NASM installation,
+REM set the installation path here:
+SET NASM=C:\Tools\mingw32
 
 REM === CONFIG END =======================================
-TITLE ImpLib SDK
-echo Building an executable using the MSVCRT library with NASM and Polink
-echo.
+TITLE ImpLib SDK (NASM MSVCRT example)
 SET NASMEXE="%NASM%\nasmw"
 IF EXIST "%NASMEXE%.exe" GOTO NASMFOUND
 SET NASMEXE="%NASM%\nasm"
@@ -22,11 +21,22 @@ GOTO EXIT
 :NASMFOUND
 "%NASMEXE%" -fwin32 test.asm
 SET POLINK="%NASM%\polink"
-IF EXIST "%POLINK%.exe" GOTO LINKFOUND
-ECHO Polink linker not found.
+IF EXIST "%POLINK%.exe" GOTO POLINKFOUND
+SET POLINK="%NASM%\bin\polink"
+IF EXIST "%POLINK%.exe" GOTO POLINKFOUND
+SET LDLINK="%NASM%\ld"
+IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
+SET LDLINK="%NASM%\bin\ld"
+IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
+ECHO No linker found (neither Polink nor LD).
 GOTO EXIT
-:LINKFOUND
+:POLINKFOUND
+echo Linking the Win32 executable to MSVCRT.DLL using Polink
 "%POLINK%" /ENTRY:start /SUBSYSTEM:CONSOLE /MERGE:.data=.text test.obj ..\..\..\lib\MSVCRT32\stripped\msvcrt.lib
+GOTO EXIT
+:LDLINKFOUND
+echo Linking the Win32 executable to MSVCRT.DLL using the GNU Linker (LD)
+"%LDLINK%" -m i386pe -subsystem console -o test.exe -e _start test.obj ..\..\..\lib\MSVCRT32\stripped\msvcrt.lib
 :EXIT
 pause
 ENDLOCAL
