ef8df4be45d6acd163a4d06504922cad77e52212
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-13 12:06:42 -0500

    Update build.bat to support GNU Linker

--- src/Win64/test_nasm/build.bat
+++ src/Win64/test_nasm/build.bat
@@ -2,13 +2,12 @@
 SETLOCAL
 REM === CONFIG BEGIN =====================================
 
-REM If you have NASM installed, set the installation path:
-SET NASM=C:\Tools\NASM
+REM If you have MinGW-w64 or a standalone NASM installation,
+REM set the installation path here:
+SET NASM=C:\Tools\mingw64
 
 REM === CONFIG END =======================================
-TITLE ImpLib SDK
-echo Building a Win64 executable with NASM and Polink or LD
-echo.
+TITLE ImpLib SDK (NASM Win64 example)
 SET NASMEXE="%NASM%\nasmw"
 IF EXIST "%NASMEXE%.exe" GOTO NASMFOUND
 SET NASMEXE="%NASM%\nasm"
@@ -23,14 +22,20 @@ GOTO EXIT
 "%NASMEXE%" -fwin64 test.asm
 SET POLINK="%NASM%\polink"
 IF EXIST "%POLINK%.exe" GOTO POLINKFOUND
+SET POLINK="%NASM%\bin\polink"
+IF EXIST "%POLINK%.exe" GOTO POLINKFOUND
 SET LDLINK="%NASM%\ld"
+IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
+SET LDLINK="%NASM%\bin\ld"
 IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
 ECHO No linker found (neither Polink nor LD).
 GOTO EXIT
 :POLINKFOUND
+echo Linking the Win64 executable using Polink
 "%POLINK%" /ENTRY:start /SUBSYSTEM:CONSOLE /MERGE:.data=.text /LIBPATH:..\..\..\lib\Win64\stripped test.obj kernel32.lib
 GOTO EXIT
 :LDLINKFOUND
+echo Linking the Win64 executable using the GNU Linker (LD)
 "%LDLINK%" -m i386pep -subsystem console -o test.exe -e start -L..\..\..\lib\Win64\stripped test.obj -lkernel32 -luser32
 :EXIT
 pause
