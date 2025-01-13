75a41c61e0e26662f76e3ab49a268443e7cbfcca
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-13 12:05:06 -0500

    Update build.bat to support GNU Linker

--- src/Win32/test_nasm/build.bat
+++ src/Win32/test_nasm/build.bat
@@ -2,13 +2,12 @@
 SETLOCAL
 REM === CONFIG BEGIN =====================================
 
-REM If you have NASM installed, set the installation path:
-SET NASM=C:\Tools\NASM
+REM If you have MinGW32 or a standalone NASM installation,
+REM set the installation path here:
+SET NASM=C:\Tools\mingw32
 
 REM === CONFIG END =======================================
-TITLE ImpLib SDK
-echo Building a Win32 executable with NASM and Polink or LD
-echo.
+TITLE ImpLib SDK (NASM Win32 example)
 SET NASMEXE="%NASM%\nasmw"
 IF EXIST "%NASMEXE%.exe" GOTO NASMFOUND
 SET NASMEXE="%NASM%\nasm"
@@ -23,15 +22,21 @@ GOTO EXIT
 "%NASMEXE%" -fwin32 test.asm
 SET POLINK="%NASM%\polink"
 IF EXIST "%POLINK%.exe" GOTO POLINKFOUND
+SET POLINK="%NASM%\bin\polink"
+IF EXIST "%POLINK%.exe" GOTO POLINKFOUND
 SET LDLINK="%NASM%\ld"
+IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
+SET LDLINK="%NASM%\bin\ld"
 IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
 ECHO No linker found (neither Polink nor LD).
 GOTO EXIT
 :POLINKFOUND
+echo Linking the Win32 executable using Polink
 "%POLINK%" /ENTRY:start /SUBSYSTEM:WINDOWS /MERGE:.data=.text /LIBPATH:..\..\..\lib\Win32\stripped test.obj kernel32.lib user32.lib
 GOTO EXIT
 :LDLINKFOUND
-"%LDLINK%" -m i386pe -subsystem windows -o test.exe -e start -L..\..\..\lib\Win32\stripped test.obj -lkernel32 -luser32
+echo Linking the Win32 executable using the GNU Linker (LD)
+"%LDLINK%" -m i386pe -subsystem windows -o test.exe -e _start -L..\..\..\lib\Win32\stripped test.obj -lkernel32 -luser32
 :EXIT
 pause
 ENDLOCAL
