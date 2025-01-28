3a2bc3b5adcd0c04a52d2540adbf98454069ac20
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-28 10:56:58 -0500

    Updated NASM Win32 example

--- src/Win32/test_nasm/test.asm
+++ src/Win32/test_nasm/test.asm
@@ -1,5 +1,7 @@
+EXTERN __imp__Beep@8
 EXTERN __imp__MessageBoxA@16
 EXTERN __imp__ExitProcess@4
+%define Beep [__imp__Beep@8]
 %define MessageBox [__imp__MessageBoxA@16]
 %define ExitProcess [__imp__ExitProcess@4]
 
@@ -9,10 +11,13 @@ msg db "Hello, world!",0
 align 4
 GLOBAL _start
 _start:
-	push 0
-	push ttl
-	push msg
-	push 0
+	push 300   ; dwDuration: 300 ms
+	push 750   ; dwFreq: 750 Hz
+	call Beep
+	push 0     ; uType: MB_OK
+	push ttl   ; lpCaption
+	push msg   ; lpText
+	push 0     ; hWnd: no owner window
 	call MessageBox
-	push eax
+	push eax   ; uExitCode
 	call ExitProcess
