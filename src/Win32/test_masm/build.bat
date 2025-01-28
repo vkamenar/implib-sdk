ec52af0158112dc6780ad230353efc0e1253d16a
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-28 10:58:35 -0500

    Updated MASM Win32 example

--- src/Win32/test_masm/build.bat
+++ src/Win32/test_masm/build.bat
@@ -23,7 +23,7 @@ IF EXIST "%MSLNK%.exe" GOTO LINKFOUND
 ECHO Linker not found.
 GOTO EXIT
 :LINKFOUND
-"%MSLNK%" /SUBSYSTEM:WINDOWS /MERGE:.rdata=.text /LIBPATH:..\..\..\lib\Win32\stripped -ignore:4078 test.obj
+"%MSLNK%" /SAFESEH:NO /SUBSYSTEM:WINDOWS /MERGE:.rdata=.text /LIBPATH:..\..\..\lib\Win32\stripped -ignore:4078 test.obj
 :EXIT
 pause
 ENDLOCAL
