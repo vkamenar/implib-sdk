f3d3a4e7c73bfa53498da9e084ebec2e81db23e0
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-28 08:02:30 -0500

    Add LLVM example

---
+++ src/Win32/test_llvm/build.bat
@@ -0,0 +1,31 @@
+@echo off
+SETLOCAL
+REM === CONFIG BEGIN =====================================
+
+REM If LLVM is installed, set the installation path here:
+SET LLVM="C:\Program Files (x86)\LLVM"
+
+REM === CONFIG END =======================================
+TITLE ImpLib SDK (LLVM Win32 example)
+SET CLANGEXE="%LLVM%\clang"
+IF EXIST "%CLANGEXE%.exe" GOTO CLANGFOUND
+SET CLANGEXE="%LLVM%\bin\clang"
+IF EXIST "%CLANGEXE%.exe" GOTO CLANGFOUND
+ECHO CLANG not found. Please, specify the correct path, if LLVM is installed.
+GOTO EXIT
+:CLANGFOUND
+"%CLANGEXE%" -c test.c -o test.o
+IF %ERRORLEVEL% NEQ 0 GOTO EXIT
+SET LLDLINK="%LLVM%\lld-link"
+IF EXIST "%LLDLINK%.exe" GOTO LLDLNKFOUND
+SET LLDLINK="%LLVM%\bin\lld-link"
+IF EXIST "%LLDLINK%.exe" GOTO LLDLNKFOUND
+ECHO LLD linker not found.
+GOTO EXIT
+:LLDLNKFOUND
+echo Linking the Win32 executable using the LLD linker
+"%LLDLINK%" /SAFESEH:NO /SUBSYSTEM:WINDOWS /ENTRY:start /LIBPATH:..\..\..\lib\Win32 test.o kernel32.lib user32.lib
+:EXIT
+pause
+ENDLOCAL
+@echo on
