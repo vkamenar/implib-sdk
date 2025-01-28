9ce9d1c106752df08011a46af0a4966d80af623f
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-28 10:57:36 -0500

    Updated MASM Win32 example

--- src/Win32/test_masm/test.asm
+++ src/Win32/test_masm/test.asm
@@ -4,6 +4,7 @@
 includelib user32.lib
 includelib kernel32.lib
 
+Beep        PROTO STDCALL :DWORD, :DWORD
 MessageBoxA PROTO STDCALL :DWORD, :DWORD, :DWORD, :DWORD
 ExitProcess PROTO STDCALL :DWORD
 
@@ -12,6 +13,7 @@ ttl db "Test",0
 msg db "Hello, world!",0
 align 4
 start:
+	invoke Beep, 750, 300 ; 750Hz ~300 ms
 	invoke MessageBoxA, 0, OFFSET msg, OFFSET ttl, 0
 	invoke ExitProcess, eax
 	retn
