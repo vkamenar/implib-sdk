477a5b3f1bd0a5d1304c20f99477d4e6b31b3bc0
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-28 11:08:30 -0500

    Updated GCC Win32 example

--- src/Win32/test_gcc/test.c
+++ src/Win32/test_gcc/test.c
@@ -1,8 +1,10 @@
 // Most of the Win32 API uses the STDCALL calling convention
+__stdcall int Beep(int, int);
 __stdcall int MessageBoxA(int, char*, char*, int);
 __stdcall int ExitProcess(int);
 
 void start(char *argv0){
+	Beep(750, 300); // 750 Hz ~300 ms
 	MessageBoxA(0, "Hello, world!", "Test", 0);
 	ExitProcess(0);
 }
