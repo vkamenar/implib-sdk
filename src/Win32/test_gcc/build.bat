8bce1f781c592e3570200408546391b59389034d
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-01-31 12:30:08 -0500

    Added optional file size optimization flags

--- src/Win32/test_gcc/build.bat
+++ src/Win32/test_gcc/build.bat
@@ -3,7 +3,7 @@ SETLOCAL
 REM === CONFIG BEGIN =====================================
 
 REM If MinGW32 is installed, set the installation path here:
-SET GCC=C:\Tools\mingw32
+SET GCC="C:\Tools\mingw32"
 
 REM === CONFIG END =======================================
 TITLE ImpLib SDK (GCC Win32 example)
@@ -14,7 +14,12 @@ IF EXIST "%GCCEXE%.exe" GOTO GCCFOUND
 ECHO GCC not found. Please, specify the correct path, if MinGW32 is installed.
 GOTO EXIT
 :GCCFOUND
+
+REM Optional file size optimization flags:
+REM   To remove EH sections: -fomit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables -fno-unwind-tables
+REM   To remove the GCC version information: -fno-ident
 "%GCCEXE%" -c test.c -o test.o
+
 IF %ERRORLEVEL% NEQ 0 GOTO EXIT
 SET LDLINK="%GCC%\ld"
 IF EXIST "%LDLINK%.exe" GOTO LDLINKFOUND
@@ -24,7 +29,12 @@ ECHO GNU linker not found.
 GOTO EXIT
 :LDLINKFOUND
 echo Linking the Win32 executable using the GNU Linker (LD)
+
+REM Optional file size optimization flags:
+REM   To remove symbol tables (debug directory): -s
+REM   To remove the .reloc section: --disable-reloc-section
 "%LDLINK%" -m i386pe -subsystem windows -o test.exe -e _start -L..\..\..\lib\Win32\stripped test.o -lkernel32 -luser32
+
 :EXIT
 pause
 ENDLOCAL
