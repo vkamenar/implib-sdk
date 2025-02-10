c0cfd93e2f4485a096c7d89ae7bb36fdf6ff41b9
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-10 10:29:07 -0500

    Pseudorandom number generator using MSVCRT

--- src/MSVCRT32/test_gcc/test.c
+++ src/MSVCRT32/test_gcc/test.c
@@ -1,6 +1,22 @@
 // MSVCRT uses the C linkage convention (CDECL)
 __cdecl int printf(const char *, ...);
+__cdecl int _getch(void);
+__cdecl int srand(unsigned int);
+__cdecl int rand(void);
+__cdecl int time(int*);
 
 void start(char *argv0){
-	printf("Hello World!");
+	printf("Pseudorandom numbers:\n");
+
+	// Set the starting seed value for the pseudorandom
+	// number generator to the current time in seconds (UTC)
+	srand(time(0));
+
+	for(int i = 0; i < 5; i++){
+		printf("  %6d\n", rand());
+	}
+
+	// Wait for some input before exiting
+	printf("\nPress any key to exit");
+	_getch();
 }
