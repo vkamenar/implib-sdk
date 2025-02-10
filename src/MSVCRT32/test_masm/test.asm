37f75e58404949ba02492cc16e4054ecdca811a4
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-10 10:56:53 -0500

    Print current date and time using MSVCRT

--- src/MSVCRT32/test_masm/test.asm
+++ src/MSVCRT32/test_masm/test.asm
@@ -5,10 +5,28 @@
 include msvcrt.inc
 includelib msvcrt.lib
 
+.DATA
+buf   db 128 dup(0)
+sDate db "The current date is: %s",13,10,0
+sTime db "The current time is: %s",13,10,0
+sExit db "Press any key to exit",0
+
 .CODE
-format db "Hello, world!",0
-align 4
 start:
-	invoke printf,OFFSET format
+	; Set time zone from TZ environment variable.
+	; If TZ is not set, the operating system default value is used.
+	invoke _tzset
+
+	; Print the current date
+	invoke _strdate_s,OFFSET buf,128
+	invoke printf,OFFSET sDate,OFFSET buf
+
+	; Print the current time
+	invoke _strtime_s,OFFSET buf,128
+	invoke printf,OFFSET sTime,OFFSET buf
+
+	; Wait for some input before exiting
+	invoke printf,OFFSET sExit
+	invoke _getch
 	ret
 END start
