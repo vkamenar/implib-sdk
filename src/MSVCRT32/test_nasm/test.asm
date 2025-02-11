ec118f1b3304a6d2923cc64330dab87535e6e819
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-11 09:52:55 -0500

    List all disk drives using MSVCRT

--- src/MSVCRT32/test_nasm/test.asm
+++ src/MSVCRT32/test_nasm/test.asm
@@ -1,13 +1,47 @@
-; MSVCRT!printf
+; MSVCRT functions:
+EXTERN __imp___getdrives
+%define _getdrives [__imp___getdrives]
 EXTERN __imp__printf
 %define printf [__imp__printf]
+EXTERN __imp___getch
+%define _getch [__imp___getch]
 
 section .text
-format db "Hello, world!",0
+msg   db "All disk drives available:",13,10,0
+fmt   db "Drive: %c",13,10,0
+sExit db "Press any key to exit",0
 align 4
 GLOBAL _start
 _start:
-	push format
+	push ebx
+	push esi
+	push msg
 	call printf
-	add esp,4 ; fix the stack
+	pop edx ; fix the stack
+	call _getdrives
+	xor esi,esi
+	mov ebx,eax
+_loop:
+	inc esi
+	cmp esi,27
+	je _end
+	test ebx,1
+	jz _nodrive
+	lea eax,[esi+40h]
+	push eax
+	push fmt
+	call printf
+	add esp,8 ; fix the stack
+_nodrive:
+	shr ebx,1
+	jmp _loop
+_end:
+
+	; Wait for some input before exiting
+	push sExit
+	call printf
+	pop edx ; fix the stack
+	call _getch
+	pop esi
+	pop ebx
 	ret
