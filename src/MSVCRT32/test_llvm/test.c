b3df5b5f6a31f616460b3769f6c6a97ea6b5b39d
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-12 07:41:57 -0500

    Print some environment variables using MSVCRT

--- src/MSVCRT32/test_llvm/test.c
+++ src/MSVCRT32/test_llvm/test.c
@@ -1,22 +1,15 @@
 // MSVCRT uses the C linkage convention (CDECL)
 __cdecl int printf(const char *, ...);
 __cdecl int _getch(void);
-__cdecl int srand(unsigned int);
-__cdecl int rand(void);
-__cdecl int time(int*);
+__cdecl char* getenv(char*);
 
 void start(char *argv0){
-	printf("Pseudorandom numbers:\n");
 
-	// Set the starting seed value for the pseudorandom
-	// number generator to the current time in seconds (UTC)
-	srand(time(0));
-
-	for(int i = 0; i < 5; i++){
-		printf("  %6d\n", rand());
-	}
+	// Print some environment variables:
+	printf("PATH: %s\n", getenv("PATH"));
+	printf("TEMP: %s\n", getenv("TEMP"));
 
 	// Wait for some input before exiting
-	printf("\nPress any key to exit");
+	printf("Press any key to exit");
 	_getch();
 }
