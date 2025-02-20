ff18cc07a90556198a658f0ef3b57936903d206c
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-20 15:03:03 -0500

    Added VB6LINK tool detection as a prevalidation

--- src/VisualBasic6/build.bat
+++ src/VisualBasic6/build.bat
@@ -1,5 +1,5 @@
 @echo off
-TITLE ImpLib SDK sample: Visual Basic 6 with OpenAL
+TITLE ImpLib SDK sample project: Visual Basic 6 with OpenAL
 PUSHD "%~dp0"
 echo Compiling the OpenAL32 import library for Visual Basic 6
 echo.
@@ -7,9 +7,6 @@ echo.
 IF %ERRORLEVEL% NEQ 0 GOTO EXIT
 
 echo.
-echo Compiling the Visual Basic 6 sample project
-echo.
-REM Get VB6 path
 SET VB6="\Program Files\Microsoft Visual Studio\VB98"
 IF EXIST "%VB6%\vb6.exe" GOTO VB6FOUND
 SET VB6="\Program Files (x86)\Microsoft Visual Studio\VB98"
@@ -20,6 +17,12 @@ IF EXIST "%VB6%\vb6.exe" GOTO VB6FOUND
 ECHO -ERR: Visual Basic 6 not found.
 GOTO EXIT
 :VB6FOUND
+IF EXIST "%VB6%\LNK.EXE" GOTO LNKFOUND
+ECHO -ERR: VB6LINK not found in %VB6%. Please, check the readme for information on how to install VB6LINK.
+GOTO EXIT
+:LNKFOUND
+echo Compiling the Visual Basic 6 sample project
+echo.
 "%VB6%\vb6.exe" /make vb6_openal.vbp
 
 :EXIT
