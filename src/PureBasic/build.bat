11dfa0cdc58d5cc9375fa5929e0a4eba7392c519
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-23 16:25:27 -0500

    Try to detect if the PureBasic compiler is 64 bit

--- src/PureBasic/build.bat
+++ src/PureBasic/build.bat
@@ -11,16 +11,19 @@ SET PB_HOME="C:\Program Files (x86)\PureBasic"
 IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PBFOUND
 FOR /f "tokens=2*" %%i IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\PureBasic_is1" /s 2^>nul ^| find "InstallLocation"') DO SET PB_HOME=%%j
 IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PBFOUND
+FOR /f "tokens=2*" %%i IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\PureBasic (x64)_is1" /s 2^>nul ^| find "InstallLocation"') DO SET PB_HOME=%%j
+IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PB64
 ECHO -ERR: PureBasic not found. It is required to generate the User-Lib.
+GOTO EXIT
+:PB64
+ECHO -ERR: 64-bit PureBasic found, but this sample project requires a 32-bit compiler.
 GOTO EXIT
 :PBFOUND
 REM Convert to short path format. Othewise the LibraryMaker will fail.
 for %%x in ("%PB_HOME%\") do set SH_PBHOME=%%~dpsx
 echo Generating the User-Lib for PureBasic.
 echo.
-%SH_PBHOME%\SDK\LibraryMaker .\PBopenal.desc
-IF %ERRORLEVEL% NEQ 0 GOTO EXIT
-copy /Y PBopenal "%PB_HOME%\PureLibraries\UserLibraries\PBopenal"
+%SH_PBHOME%\SDK\LibraryMaker .\PBopenal.desc /TO %SH_PBHOME%\PureLibraries\UserLibraries\
 IF %ERRORLEVEL% NEQ 0 GOTO EXIT
 
 echo.
