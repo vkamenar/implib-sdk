3df543922d66be63ae3f483b827b658be45fd06b
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-24 11:01:01 -0500

    Improved PB installation directory detection

--- src/PureBasic/build.bat
+++ src/PureBasic/build.bat
@@ -7,17 +7,27 @@ echo.
 IF %ERRORLEVEL% NEQ 0 GOTO EXIT
 
 echo.
-SET PB_HOME="C:\Program Files (x86)\PureBasic"
+REM Try to locate the PureBasic (32-bit) installation directory
+SET PB64=0
+SET PB_HOME=%ProgramFiles(X86)%
+IF "%ProgramFiles(X86)%" == "" SET PB_HOME=%ProgramFiles%
+SET PB_HOME=%PB_HOME%\PureBasic
+IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PBFOUND
+FOR /f "tokens=2*" %%i IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\PureBasic (x86)_is1" /s 2^>nul ^| find "InstallLocation"') DO SET PB_HOME=%%j
 IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PBFOUND
 FOR /f "tokens=2*" %%i IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\PureBasic_is1" /s 2^>nul ^| find "InstallLocation"') DO SET PB_HOME=%%j
 IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PBFOUND
+
+REM Check if 64-bit version is available
+SET PB64=1
+SET PB_HOME=%ProgramW6432%\PureBasic
+IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PBFOUND
 FOR /f "tokens=2*" %%i IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\PureBasic (x64)_is1" /s 2^>nul ^| find "InstallLocation"') DO SET PB_HOME=%%j
-IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PB64
+IF EXIST "%PB_HOME%\SDK\LibraryMaker.exe" GOTO PBFOUND
+
 ECHO -ERR: PureBasic not found. It is required to generate the User-Lib.
 GOTO EXIT
-:PB64
-ECHO -ERR: 64-bit PureBasic found, but this sample project requires a 32-bit compiler.
-GOTO EXIT
+
 :PBFOUND
 REM Convert to short path format. Othewise the LibraryMaker will fail.
 for %%x in ("%PB_HOME%\") do set SH_PBHOME=%%~dpsx
@@ -26,6 +36,11 @@ echo.
 %SH_PBHOME%\SDK\LibraryMaker .\PBopenal.desc /TO %SH_PBHOME%\PureLibraries\UserLibraries\
 IF %ERRORLEVEL% NEQ 0 GOTO EXIT
 
+IF %PB64% == 0 GOTO BUILD_EXE
+ECHO -WNG: The User-Lib was created, but this sample project requires a 32-bit PureBasic compiler to create the executable.
+GOTO EXIT
+
+:BUILD_EXE
 echo.
 echo Compiling the sample PureBasic application
 echo.
