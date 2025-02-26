489adf11e0437f5327a9e03c751efce58186f06c
Vladimir Kameñar <69826282+vkamenar@users.noreply.github.com>
2025-02-26 16:19:47 -0500

    Added optional file size optimization flags

--- src/Win64/test_masm/build.bat
+++ src/Win64/test_masm/build.bat
@@ -3,11 +3,10 @@ SETLOCAL
 REM === CONFIG BEGIN =================================================
 
 REM If you have MASM64 installed, set the installation path:
-SET MASM=C:\Tools\MASM64
+SET MASM=C:\MASM64
 
 REM === CONFIG END ===================================================
-TITLE ImpLib SDK (MASM Win64 example)
-echo Building a Win64 executable with MASM64 and either the MS linker or polink
+TITLE ImpLib SDK (MASM64 Win64 example)
 IF EXIST "%MASM%\ml64.exe" GOTO MASMFOUND
 SET MASM=%MASM%\bin
 IF EXIST "%MASM%\ml64.exe" GOTO MASMFOUND
@@ -19,13 +18,25 @@ GOTO EXIT
 "%MASM%\ml64" /c test.asm
 IF %ERRORLEVEL% NEQ 0 GOTO EXIT
 SET LNKEXE="%MASM%\link"
-IF EXIST "%LNKEXE%.exe" GOTO LINKFOUND
+IF EXIST "%LNKEXE%.exe" GOTO MSLINKFOUND
 SET LNKEXE="%MASM%\polink"
-IF EXIST "%LNKEXE%.exe" GOTO LINKFOUND
+IF EXIST "%LNKEXE%.exe" GOTO POLINKFOUND
 ECHO Linker not found.
 GOTO EXIT
-:LINKFOUND
-"%LNKEXE%" /ENTRY:start /SUBSYSTEM:CONSOLE /MACHINE:X64 /MERGE:.rdata=.text /LIBPATH:..\..\..\lib\Win64\stripped -ignore:4078 test.obj
+:MSLINKFOUND
+echo Linking the Win64 executable using MS linker
+
+REM Optional file size optimization flag:
+REM   To remove the debug directory (undocumented flag): /EMITPOGOPHASEINFO
+"%LNKEXE%" /ENTRY:start /SUBSYSTEM:CONSOLE /MACHINE:X64 /LIBPATH:..\..\..\lib\Win64 test.obj
+GOTO EXIT
+:POLINKFOUND
+echo Linking the Win64 executable using Polink
+
+REM Optional file size optimization flags:
+REM   Merge '.data' section to '.text': /MERGE:.data=.text
+"%LNKEXE%" /ENTRY:start /SUBSYSTEM:CONSOLE /MACHINE:X64 /LIBPATH:..\..\..\lib\Win64 test.obj
+
 :EXIT
 pause
 ENDLOCAL
